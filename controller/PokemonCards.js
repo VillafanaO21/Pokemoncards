fb99075118198ce8f2be8d08551d507527c0b20b
rhsb <ovillafana65@gmail.com>
2022-12-08 11:37:19 -0800

    commit 7

--- controller/PokemonCards.js
+++ controller/PokemonCards.js
@@ -1,6 +1,12 @@
 const PokemonCards = require('../controller/PokemonCards');
-const Pokemon = {
-    id: 1,
-    name: 'Tepig',
-    imgae: ''
-}
+module.exports.viewAll = function(req,res, ) {
+    const PokemonCards = {
+        id: 1,
+        name: 'Tepig',
+        imgae: 'https://staticg.sportskeeda.com/editor/2022/11/224f1-16681562150640-1920.jpg',
+        Hp: 60,
+        Type: 'Fire',
+        description:'It can deftly dodge its foe’s attacks while shooting fireballs from its nose. It roasts berries before it eats them.'
+        };
+    res.render('index', {PokemonCards});
+    }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/',);
+router.get('/', PokemonCards.viewAll);
 
 module.exports = router;
