46877c324c8d554575143ad2438e3fb8095f92e6
rhsb <ovillafana65@gmail.com>
2022-12-14 10:05:08 -0800

    commit 9

--- controller/PokemonCards.js
+++ controller/PokemonCards.js
@@ -1,5 +1,14 @@
-const PokemonCards = require('../controller/PokemonCards');
+const {PokemonCards} = require('../models')
+const health = [60];
 module.exports.viewAll = async function(req,res, ) {
     const PokemonCards = await PokemonCards.finAll();
     res.render('index', {PokemonCards});
+}
+router.get('/edit/:id', PokemonCards.renderEditForm);
+
+module.exports.renderEditForm = async function(req, res,) {
+    const PokemonCards = await PokemonCards.findByPk(
+        req.params.id
+    );
+    res.render('edit', {PokemonCards, health});
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const PokemonCards = require('../controller/PokemonCards');
 
 /* GET home page. */
 router.get('/', PokemonCards.viewAll);
+router.get('/edit', PokemonCards.renderEditForm);
 
 module.exports = router;
